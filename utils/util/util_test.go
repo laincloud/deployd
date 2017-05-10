58625507cd860fed5d0d2345b742250f157638ea
wenbinmeng <wenbinmeng@creditease.cn>
2017-05-10 17:23:34 +0800

    Fixed bug for streamrouter dstport upgdate

--- utils/util/util_test.go
+++ utils/util/util_test.go
@@ -2,7 +2,9 @@ package util
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func Test_ParseNameInstanceNo(t *testing.T) {
@@ -12,4 +14,16 @@ func Test_ParseNameInstanceNo(t *testing.T) {
 	} else {
 		fmt.Println("err:%v\n", err)
 	}
+}
+
+func Test_deepEqual(t *testing.T) {
+	a := []string{"a", "b"}
+	b := []string{"a", "b"}
+	fmt.Println(reflect.DeepEqual(a, b))
+}
+
+func Test_timeFormat(t *testing.T) {
+	fmt.Println(time.Now().Format("Jan  2 15:04:05"))
+	// Jan  1 00:00:00
+	// time.Now().Format("2006-01-02 15:04:05")
 }
