0e6fff7ddcf7d43694a38c8e71e602b4c15cfb8e
YuYang <yyangplus@gmail.com>
2018-04-10 11:23:38 +0800

    add more information to version output

---
+++ version/version.go
@@ -0,0 +1,22 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/coreos/go-semver/semver"
+)
+
+var (
+	Version    = "2.4.1+git"
+	APIVersion = "unknown"
+
+	// Git SHA Value will be set during build
+	GitSHA = "Not provided (use ./build instead of go build)"
+)
+
+func init() {
+	ver, err := semver.NewVersion(Version)
+	if err == nil {
+		APIVersion = fmt.Sprintf("%d.%d", ver.Major, ver.Minor)
+	}
+}
